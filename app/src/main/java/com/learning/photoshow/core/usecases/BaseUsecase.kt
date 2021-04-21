bfba703fbf1fc50267aa17de62845ea5aff22db5
ramzyhassan44 <ramzyhassan44@gmail.com>
2021-04-21 06:31:58 +0200

    add coroutines dependancy add base usecase and resultoutput class

---
+++ app/src/main/java/com/learning/photoshow/core/usecases/BaseUsecase.kt
@@ -0,0 +1,16 @@
+package com.learning.photoshow.core.usecases
+
+import com.learning.photoshow.core.data.ResultOutput
+
+abstract class BaseProcessor<T : Any> {
+    suspend fun execute(): ResultOutput<T> = try {
+        validate()
+        process()
+    } catch (exception: Exception) {
+        ResultOutput.ErrorResult(exception)
+    }
+
+    abstract suspend fun validate()
+
+    abstract suspend fun process(): ResultOutput<T>
+}
